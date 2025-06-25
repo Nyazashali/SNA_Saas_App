0290584ec3e8da2479ba5c90c88daa2cee5650de
Nyazashali <shaik.nyazashali@gmail.com>
2025-06-25 19:22:42 +0530

    Companions.action Supabase Imp

--- lib/actions/companion.action.ts
+++ lib/actions/companion.action.ts
@@ -3,6 +3,8 @@
 
 import { auth } from "@Clerk/nextjs/server";
 import { createSupabaseClient } from "@/lib/supabase";
+import { create } from "domain";
+
 
 export const createCompanion = async(formData: CreateCompanion) => {
     const { userId: author} = await auth();
@@ -15,4 +17,29 @@ export const createCompanion = async(formData: CreateCompanion) => {
 
     if (error || !data) throw new Error(error.message || "Failed to create companion");
     return data[0];
+}
+
+export const getAllCompanions = async({ limit = 10, page = 1, subject, topic}: GetAllCompanions) => {
+    const supabase = createSupabaseClient();
+
+    let query = supabase
+        .from(relation:'companions').select();
+
+        if(subject && topic){
+            query = query.ilike(column:'subject', pattern: `%${subject}%`)
+            .or(filters:`topic.ilike.%${topic}%,name.ilike.%${topic}%`);
+        } else if (subject) {
+            query = query.ilike(column:'subject', pattern: `%${subject}%`);
+        } else if (topic) {
+            query = query.or(filters:`topic.ilike.%${topic}%,name.ilike.%${topic}%`);
+        }
+
+        query = query.range((page - 1) * limit, page * limit - 1);
+
+        const { data: companions, error} = await query;
+        if(error) throw new Error(error.message);
+
+        return companions;
+
+
 }
